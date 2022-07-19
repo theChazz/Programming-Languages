a614475aa3ab65534f7c50258380740eaf03ec1f
theChazz <kasgem@gmail.com>
2022-07-19 09:26:25 +0200

    Added a new API using sql cmd

---
+++ C-Sharp/API's/WebAPI1.01 SQL Commands/WebAPI1.01/WebAPI1.01/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace WebAPI1._01.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ C-Sharp/API's/WebAPI1.01 SQL Commands/WebAPI1.01/WebAPI1.01/Models/EmployeeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI1._01.Models
+{
+    public class EmployeeModel
+    {
+            public int EmployeeId { get; set; }
+            public string Email { get; set; }
+            public string Salary { get; set; }
+        
+    }
+}
